499e270489af68d0fe4219f169e5e99045a0b2e1
Mesa1234567789 <Masapimonpat@gmail.com>
2025-12-06 23:09:00 +0700

    8

--- main.js
+++ main.js
@@ -464,4 +464,19 @@ document.addEventListener('DOMContentLoaded', () => {
       btn.textContent = 'SUBMIT';
     }
   });
+  document.addEventListener("DOMContentLoaded", () => {
+  const items = document.querySelectorAll(".fade-up");
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+        observer.unobserve(entry.target); // แสดงครั้งเดียว
+      }
+    });
+  }, { threshold: 0.2 });
+
+  items.forEach(item => observer.observe(item));
+});
+
 });
